c174ddce5e6dc44d4e54fd1284c2c41471816684
GitHub Action <action@github.com>
2021-10-29 08:59:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pcloves/lombokintellijpluginextend/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pcloves.lombokintellijpluginextend.services
+
+import com.github.pcloves.lombokintellijpluginextend.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
